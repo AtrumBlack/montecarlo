7f6ba058ff7e877d1dda1335ed1927228fd5fd7b
AtrumBlack <walterbasanta66@gmail.com>
2024-01-28 15:00:16 -0300

    act

--- js/cargar_contenido.js
+++ js/cargar_contenido.js
@@ -3,31 +3,26 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Cargar contenido de menu.html
     const menuContainer = document.getElementById('menuContainer');
-    fetch('carga_inicial/menu.html')
+    fetch('/carga_inicial/menu.html')
         .then(response => response.text())
         .then(data => menuContainer.innerHTML = data);
 
-    // // Cargar contenido de inicio_carrusel.html
-    // const carouselContainer = document.getElementById('carouselContainer');
-    // fetch('inicio_carousel.html')
-    //     .then(response => response.text())
-    //     .then(data => carouselContainer.innerHTML = data);
 
     // Cargar contenido de habitaciones_card.html
     const habitacionesContainer = document.getElementById('habitacionesContainer');
-    fetch('carga_inicial/habitaciones_card.html')
+    fetch('/carga_inicial/habitaciones_card.html')
         .then(response => response.text())
         .then(data => habitacionesContainer.innerHTML = data);
 
     // Cargar contenido de video_presentacion.html
     const videoContainer = document.getElementById('contenidoInicio');
-    fetch('carga_inicial/video_inicio.html')
+    fetch('/carga_inicial/video_inicio.html')
         .then(response => response.text())
         .then(data => videoContainer.innerHTML = data);
 
     // Cargar contenido de pie.html
     const pieContainer = document.getElementById('pieContainer');
-    fetch('carga_inicial/pie.html')
+    fetch('/carga_inicial/pie.html')
         .then(response => response.text())
         .then(data => pieContainer.innerHTML = data);
 });
