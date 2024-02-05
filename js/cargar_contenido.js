5b5841cbd8aac16824a4c4d5e5f25973933ba87c
AtrumBlack <walterbasanta66@gmail.com>
2024-02-05 14:28:35 -0300

    actualizacion

--- js/cargar_contenido.js
+++ js/cargar_contenido.js
@@ -8,22 +8,33 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(response => response.text())
         .then(data => menuContainer.innerHTML = data);
 
+        // cargarInicioJS();
     // // Cargar contenido de habitaciones_card.html
     // const habitacionesContainer = document.getElementById('habitacionesContainer');
     // fetch(`${carpetaPrueba}/carga_inicial/habitaciones_card.html`)
     //     .then(response => response.text())
     //     .then(data => habitacionesContainer.innerHTML = data);
 
-    // Cargar contenido de video_inicio.html
-    const videoContainer = document.getElementById('contenidoInicio');
+    // // Cargar contenido de video_inicio.html
+    // const videoContainer = document.getElementById('contenidoInicio');
     // fetch(`${carpetaPrueba}/carga_inicial/video_inicio.html`)
-    fetch(`${carpetaPrueba}/carga_inicial/inicio.html`)
-        .then(response => response.text())
-        .then(data => videoContainer.innerHTML = data);
+    // fetch(`${carpetaPrueba}/carga_inicial/inicio.html`)
+    //     .then(response => response.text())
+    //     .then(data => videoContainer.innerHTML = data);
+
+
+
 
     // Cargar contenido de pie.html
     const pieContainer = document.getElementById('pieContainer');
     fetch(`${carpetaPrueba}/carga_inicial/pie.html`)
         .then(response => response.text())
         .then(data => pieContainer.innerHTML = data);
+
+    // // Función para cargar inicio.js
+    // function cargarInicioJS() {
+    //     const scriptInicio = document.createElement('script');
+    //     scriptInicio.src = `${carpetaPrueba}/inicio.js`;
+    //     document.body.appendChild(scriptInicio);
+    // }
 });
--- js/script.js
+++ js/script.js
@@ -6,17 +6,64 @@ document.addEventListener('DOMContentLoaded', function () {
     const inicioContainer = document.getElementById('contenidoInicio');
     const habitacionesContainer = document.getElementById('habitacionesContainer');
 
+    const imagenes = [
+        { ruta: "/img/inicio/fotos12.png", texto: "En el sur de la ciudad de Mar del Plata, es posible disfrutar de la serenidad más exquisita. Montecarlo Hotel y Apart, se encuentra ubicado a 200mtrs de la playa, muy cerca del bosque en una exclusiva zona residencial, donde el aire puro es nuestra mejor compañía." },
+        { ruta: "/img/inicio/fotos01Bis.png", texto: "En el sur de la ciudad de Mar del Plata, es posible disfrutar de la serenidad más exquisita. Montecarlo Hotel y Apart, se encuentra ubicado a 200mtrs de la playa, muy cerca del bosque en una exclusiva zona residencial, donde el aire puro es nuestra mejor compañía." },
+        { ruta: "/img/inicio/fotos11bis.png", texto: "En el sur de la ciudad de Mar del Plata, es posible disfrutar de la serenidad más exquisita. Montecarlo Hotel y Apart, se encuentra ubicado a 200mtrs de la playa, muy cerca del bosque en una exclusiva zona residencial, donde el aire puro es nuestra mejor compañía." },
+        { ruta: "/img/inicio/fotos15Bis.png", texto: "En el sur de la ciudad de Mar del Plata, es posible disfrutar de la serenidad más exquisita. Montecarlo Hotel y Apart, se encuentra ubicado a 200mtrs de la playa, muy cerca del bosque en una exclusiva zona residencial, donde el aire puro es nuestra mejor compañía." },
+        { ruta: "/img/inicio/fotos05Bis.png", texto: "En el sur de la ciudad de Mar del Plata, es posible disfrutar de la serenidad más exquisita. Montecarlo Hotel y Apart, se encuentra ubicado a 200mtrs de la playa, muy cerca del bosque en una exclusiva zona residencial, donde el aire puro es nuestra mejor compañía." },
+        { ruta: "/img/inicio/fotos13bis.png", texto: "En el sur de la ciudad de Mar del Plata, es posible disfrutar de la serenidad más exquisita. Montecarlo Hotel y Apart, se encuentra ubicado a 200mtrs de la playa, muy cerca del bosque en una exclusiva zona residencial, donde el aire puro es nuestra mejor compañía." }
+    ];
     // Cargar el archivo JSON con la información de las habitaciones
     fetch(`${carpetaPrueba}/json/habitaciones.json`)
         .then(response => response.json())
         .then(data => {
             const habitaciones = data.habitaciones;
 
-            function cargarContenido(opcion) {
+            function cargarContacto(opcion) {
                 fetch(`${carpetaPrueba}/${opcion}`)
                     .then(response => response.text())
                     .then(data => inicioContainer.innerHTML = data);
+            };
+            function cargarContenido() {
+                inicioContainer.innerHTML = `
+                <div id="carouselMontecarlo" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        ${imagenes.map(crearCarouselItem).join('')}
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselMontecarlo" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselMontecarlo" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                    <div class="carousel-indicators">
+                        ${imagenes.map((_, index) => crearIndicatorButton(index)).join('')}
+                    </div>
+                </div>
+
+                 `;
+
             }
+
+            // Función para crear elementos del carousel
+            const crearCarouselItem = (imagen, index) => `
+                <div class="carousel-item ${index === 0 ? 'active' : ''}">
+                    <img src="${imagen.ruta}" class="d-block w-100" alt="foto${index + 1}">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p>${imagen.texto}</p>
+                    </div>
+                </div>
+            `;
+
+            // Función para crear indicadores del carousel
+            const crearIndicatorButton = (index) => `
+                <button type="button" data-bs-target="#carouselMontecarlo" 
+                        data-bs-slide-to="${index}" class="${index === 0 ? 'active' : ''}"></button>
+            `;
+
 
             function cargarHabitacion(tipoHabitacion) {
                 const habitacion = habitaciones[tipoHabitacion];
@@ -128,7 +175,6 @@ document.addEventListener('DOMContentLoaded', function () {
                             `;
             }
 
-
             // Ahora, también puedes utilizar esta función para cargar habitaciones desde los botones en la sección de habitaciones.
             function cargarHabitacionDesdeBoton(event) {
                 if (event.target.hasAttribute('data-tipo-habitacion')) {
@@ -136,8 +182,6 @@ document.addEventListener('DOMContentLoaded', function () {
                     cargarHabitacion(tipoHabitacion);
                 }
             }
-
-
 
             // Evento de clic en el menú para cambiar el contenido de la sección 'inicio' o 'habitaciones'
             document.getElementById('menuContainer').addEventListener('click', function (event) {
@@ -158,12 +202,12 @@ document.addEventListener('DOMContentLoaded', function () {
                     switch (opcion) {
                         case 'inicio':
                             //   alert('inicio');
-                            cargarContenido(`carga_inicial/inicio.html`);
+                            cargarContenido();
                             // Desplazar la página al principio después de cargar el contenido
                             window.scrollTo(0, 0);
                             // Agregar clases específicas para estilos
                             // document.getElementById('contenidoInicio').classList.add('inicio-styles');
-                            document.getElementById('carouselMontecarlo').classList.add('inicio-styles');
+                            // document.getElementById('carouselMontecarlo').classList.add('inicio-styles');
 
                             break;
                         case 'galeria':
@@ -218,7 +262,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
                             break;
                         case 'contacto':
-                            cargarContenido('contactos/contacto.html');
+                            cargarContacto('contactos/contacto.html');
                             // Desplazar la página al principio después de cargar el contenido
                             window.scrollTo(0, 0);
                             break;
@@ -231,9 +275,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
             // Evento de clic en los botones "Ver Habitación" en la sección de habitaciones
             document.getElementById('habitacionesContainer').addEventListener('click', cargarHabitacionDesdeBoton);
+            cargarContenido();
+            // Llama a la función para cargar las habitaciones al cargar la página
+            cargarHabitacionesCard();
 
-                        // Llama a la función para cargar las habitaciones al cargar la página
-                        cargarHabitacionesCard();
+
 
         })
         .catch(error => console.error('Error al cargar los datos de habitaciones:', error));
